ae3f8b0f36e94af31403334c5c7e5807e8939f75
ntzannes <33675386+ntzannes@users.noreply.github.com>
2019-11-25 20:02:05 -0500

    Add files via upload

---
+++ jsChallengesWithAnswers/js/challenge1.js
@@ -0,0 +1,19 @@
+$("#subscribe").click(function(){
+		console.log("toggling email field")
+		$("#emailField").toggle("fast");
+});
+
+//
+// subscribe.onchange = function() {
+// 	var x = document.querySelector("#emailField");
+// 	if (this.checked){
+// 		console.log("selected");
+//     	x.style.display = "block";
+//   	} else {
+//   		console.log("Not selected");
+//     	x.style.display = "none";
+//   	}
+//   // 	Advanced Option....
+//   // 	x.style.display == "block" ?
+// 		// x.style.display = "none" :
+// 		// x.style.display = "block";
